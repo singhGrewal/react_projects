a7d27cc8f72997ef7dd0efe3f5f1ccae6517c231
Tejveer <tejveer.pro@gmail.com>
2024-04-30 11:41:48 -0700

    Update app

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import RandomColors from "./components/randomColors";
 import StarRating from "./components/starRating";
 import ImageSlider from "./components/imageSlider";
 import TreeView from "./components/treeView";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="">
-                <Routes>
-                    <Route exact path="/" element={<Home/> } />
-                    <Route exact path="/accordion" element={<Accordion/> } />
-                    <Route exact path="/random_colors" element={<RandomColors/> } />
-                    <Route exact path="/star_rating" element={<StarRating/> } />
-                    <Route exact path="/image_slider" element={<ImageSlider/> } />
-                    <Route exact path="/tree_view" element={<TreeView/> }/>
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/accordion" element={<Accordion/>}/>
+            <Route exact path="/random_colors" element={<RandomColors/>}/>
+            <Route exact path="/star_rating" element={<StarRating/>}/>
+            <Route exact path="/image_slider"
+                   element={<ImageSlider url={'https://picsum.photos/v2/list'} page={1} limit={5}/>}/>
+            <Route exact path="/tree_view" element={<TreeView/>}/>
+        </Routes>
     );
 }
 
