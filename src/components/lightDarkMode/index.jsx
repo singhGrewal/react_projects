5ade805d098c1445d88923fc719b62db6d03b242
Tejveer <tejveer.pro@gmail.com>
2024-05-01 21:54:08 -0700

    Fixed theme error

--- src/components/lightDarkMode/index.jsx
+++ src/components/lightDarkMode/index.jsx
@@ -3,7 +3,7 @@ import UseThemeToggle from "../../customHook/useThemeToggle";
 export default function LightDarkMode() {
     const [theme, toggleTheme] = UseThemeToggle()
 
-    return <div className={'light-dark-mode'} data-theme={theme}>
+    return <div className={'light-dark-mode'} data-theme={theme || 'light'}>
         <div className='container'>
             <p>Theme is set to : {theme}</p>
             <button onClick={toggleTheme}>Change Theme</button>
